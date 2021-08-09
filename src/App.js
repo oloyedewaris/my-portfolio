4276e8f09a033af261aadfa1c10142ef721d7a9c
Waris Oloyede <oloyedewaris@gmail.com>
2021-08-09 02:22:20 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Header from "./components/Header";
 import About from "./components/About";
 import Resume from "./components/Resume";
