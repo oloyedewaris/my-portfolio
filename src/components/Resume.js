a0bcb7453b81d1d3af08e3077a2e13c22e73e5c7
Waris Oloyede <oloyedewaris@gmail.com>
2022-09-25 03:30:48 +0100

    new

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,12 +2,82 @@ import React, { useState } from "react";
 import { LaptopOutlined, TeamOutlined, DownOutlined, UpOutlined, MobileOutlined } from "@ant-design/icons"
 
 const Resume = () => {
-  const [isOpen, setOpen] = useState({
-    frame: false,
-    ui: false,
-    devOps: false
-  })
-  const work = [
+  const categoriesData = [
+    {
+      id: 1,
+      name: "Frameworks/Languages",
+      alias: 'frame',
+      isOpen: false,
+      skills: [
+        {
+          skillname: "JavaScript (ES6+)",
+          level: "87%",
+        },
+        {
+          skillname: " ReactJS/React Native",
+          level: "92%",
+        },
+        {
+          skillname: "NextJS",
+          level: "68%",
+        },
+        {
+          skillname: "HTML5/CSS3/LESS",
+          level: "67%",
+        },
+        {
+          skillname: " NodeJS/ExpressJS",
+          level: "58%",
+        }
+      ]
+    },
+    {
+      id: 2,
+      name: "UI Libraries/Tools",
+      alias: 'ui',
+      isOpen: false,
+      skills: [
+        {
+          skillname: "Bootstrap/Material UI",
+          level: "75%"
+        },
+        {
+          skillname: "Redux/React-router/Formik",
+          level: "69%"
+        },
+        {
+          skillname: "React navigation",
+          level: "77%"
+        },
+        {
+          skillname: "Axios/Bcrypt/Json web tokens",
+          level: "75%"
+        },
+      ]
+    },
+    {
+      id: 3,
+      name: "DevOps/Web Services",
+      alias: 'devOps',
+      isOpen: false,
+      skills: [
+        {
+          skillname: "Git/Github/FireBase",
+          level: "60%"
+        },
+        {
+          skillname: "WebSockets/REST/Socket.io",
+          level: "48%"
+        },
+        {
+          skillname: " MongoDB/Mongoose",
+          level: "56%"
+        },
+      ]
+    }
+  ];
+  const [categories, setCategories] = useState(categoriesData)
+  const works = [
     {
       icon: <LaptopOutlined />,
       service: "Web development",
@@ -30,79 +100,17 @@ const Resume = () => {
     }
   ];
 
-  const skill = [
-    {
-      name: "Frameworks/Languages",
-      alias: 'frame',
-      skills: [
-        {
-          skillname: "JavaScript (ES6+)",
-          level: "87%"
-        },
-        {
-          skillname: " ReactJS/React Native",
-          level: "92%"
-        },
-        {
-          skillname: "NextJS",
-          level: "68%"
-        },
-        {
-          skillname: "HTML5/CSS3/LESS",
-          level: "67%"
-        },
-        {
-          skillname: " NodeJS/ExpressJS",
-          level: "58%"
+  const handleCollapse = id => {
+    const newCategory = [...categories]
+      .map(category => {
+        if (category.id === id) {
+          category.isOpen = !category.isOpen
+        } else {
+          category.isOpen = false
         }
-      ]
-    },
-    {
-      name: "UI Libraries/Tools",
-      alias: 'ui',
-      skills: [
-        {
-          skillname: "Bootstrap/Material UI",
-          level: "75%"
-        },
-        {
-          skillname: "Redux/React-router/Formik",
-          level: "69%"
-        },
-        {
-          skillname: "React navigation",
-          level: "77%"
-        },
-        {
-          skillname: "Axios/Bcrypt/Json web tokens",
-          level: "75%"
-        },
-      ]
-    },
-    {
-      name: "DevOps/Web Services",
-      alias: 'devOps',
-      skills: [
-        {
-          skillname: "Git/Github/FireBase",
-          level: "60%"
-        },
-        {
-          skillname: "WebSockets/REST/Socket.io",
-          level: "48%"
-        },
-        {
-          skillname: " MongoDB/Mongoose",
-          level: "56%"
-        },
-      ]
-    }
-  ];
-
-  const handleCollapse = (alias) => {
-    if (alias === 'frame') setOpen({ ui: false, devOps: false, frame: !isOpen[alias] })
-    else if (alias === 'ui') setOpen({ devOps: false, frame: false, ui: !isOpen[alias] })
-    else if (alias === 'devOps') setOpen({ frame: false, ui: false, devOps: !isOpen[alias] })
+        return category
+      })
+    setCategories(newCategory)
   }
 
   return (
@@ -114,15 +122,13 @@ const Resume = () => {
           </p>
         </div>
         <div className=" flex sm:w-4/5 flex flex-col justify-center text-center sm:text-left px-2 sm:px-4">
-          {work.map((item, i) => {
-            return (
-              <div className="block px-2 xs:px-4 py-2 my-4 hover:shadow rounded border-2" key={Math.random()}>
-                <div className="text-xl text-blue-800">{item.icon}</div>
-                <h3 className="text-xl font-medium">{item.service}</h3>
-                <p className="">{item.specialization}</p>
-              </div>
-            );
-          })}
+          {works.map((work, i) => (
+            <div className="block px-2 xs:px-4 py-2 my-4 hover:shadow rounded border-2" key={Math.random()}>
+              <div className="text-xl text-blue-800">{work.icon}</div>
+              <h3 className="text-xl font-medium">{work.service}</h3>
+              <p className="">{work.specialization}</p>
+            </div>
+          ))}
         </div>
       </div>
       <div className="mx-2 sm:mx-4 md:mx-12 sm:w-5/6 text-center block py-4 sm:py-8 sm:flex sm:justify-center">
@@ -133,34 +139,35 @@ const Resume = () => {
         </div>
         <div className="px-2 sm:px-8 sm:w-4/5 mx-auto py-auto">
           <div className="my-2">
-            {skill.map((category) => (
-              <div key={Math.random()}>
+            {categories.map((category, i) => (
+              <div key={i}>
                 <div
                   style={{ cursor: 'pointer' }}
-                  onClick={() => handleCollapse(category.alias)}
+                  onClick={() => handleCollapse(category.id)}
                   className="my-4 text-blue-800 w-full flex justify-between items-center mx-auto">
                   <span className='text-xl font-medium'>{category.name}</span>
-                  {isOpen[category.alias] ? <DownOutlined /> : <UpOutlined />}
+                  {category.isOpen ? <DownOutlined /> : <UpOutlined />}
                 </div>
-                <div>{category.skills.map((item, i) => (
-                  <>
-                    {isOpen[category.alias] ?
-                      <div className='skills_transition h-full mx-auto text-left py-2' key={Math.random()}>
-                        <div className="h-6 relative">
-                          <div className="absolute font-medium left-0">
-                            {item.skillname}
+                <div>
+                  {category.isOpen && (
+                    <div>
+                      {category.skills.map((skill, i) => (
+                        <div className='skills_transition h-full mx-auto text-left py-2' key={i}>
+                          <div className="h-6 relative">
+                            <div className="absolute font-medium left-0">
+                              {skill.skillname}
+                            </div>
+                            <div className="absolute right-0">
+                              {skill.level}
+                            </div>
                           </div>
-                          <div className="absolute right-0">
-                            {item.level}
+                          <div className="bg-blue-200 relative h-2">
+                            <div style={{ width: skill.level }} className="bg-blue-800 h-2 left-0 absolute" />
                           </div>
                         </div>
-                        <div className="bg-blue-200 relative h-2">
-                          <div style={{ width: item.level }} className="bg-blue-800 h-2 left-0 absolute" />
-                        </div>
-                      </div> :
-                      null}
-                  </>
-                ))}
+                      ))}
+                    </div>
+                  )}
                 </div>
               </div>
             ))}
